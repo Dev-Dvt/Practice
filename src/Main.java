95edb802da138320c92a8e27637eb1ed59ac3dd8
DevDvt <devangthakar02@gmail.com>
2024-07-09 14:05:09 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+// Class 2
+// Main class
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+
+        // Creating object of above class
+        Complex c1 = new Complex(10, 15);
+
+        // Following involves a copy constructor call
+        Complex c2 = new Complex(c1);
+
+        // Note: Following doesn't involve a copy
+        // constructor call
+        // as non-primitive variables are just references.
+        Complex c3 = c2;
+
+        // toString() of c2 is called here
+        System.out.println(c2);
     }
 }
